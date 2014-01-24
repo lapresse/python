6bc5f5014335d3cf6a7afc146864b285f83a4430
Marc Dufour <kafeolic@gmail.com>
2014-01-24 05:20:57 -0500

    specify default pip version

--- attributes/default.rb
+++ attributes/default.rb
@@ -32,7 +32,7 @@ else
 end
 
 default['python']['binary'] = "#{python['prefix_dir']}/bin/python"
-
+default['python']['pipversion'] = '1.3.1'
 default['python']['url'] = 'http://www.python.org/ftp/python'
 default['python']['version'] = '2.7.1'
 default['python']['checksum'] = '80e387bcf57eae8ce26726753584fd63e060ec11682d1145af921e85fd612292'
